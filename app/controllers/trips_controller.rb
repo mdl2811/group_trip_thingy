e498f9d4719a297a355cac0dbe3e0901c7d14e0d
Branden Bird <brandenjbird@gmail.com>
2017-06-03 22:02:35 -0600

    Add Trips controller and functionality

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,50 @@
+class TripsController < ApplicationController
+	before_action set_trip, only: {:show, :edit, :update, :destroy}
+
+  def index
+		@trip = current_user.trips
+  end
+
+  def show
+  end
+
+	def new
+		@trip = current_user.trips.new
+	end
+
+	def create
+		@trip = current_user.trips.new(trip_params)
+		if @trip.save
+			redirect_to @trip
+		else
+			render :new
+		end
+	end
+
+	def edit
+		render(partial: 'form', locals: {title: 'Create a new trip!'} )
+	end
+
+	def update
+		if @trip.update(trip_params)
+			redirect_to @trip
+		else
+			render edit
+		end
+	end
+
+	def destroy
+		@trip.destroy
+		redirect_to index
+	end
+
+	private
+		def trip_params
+			params.require(:trip).permit(:name)
+		end
+
+		def set_trip
+			@trip = current_user.trip(params[:id])
+		end
+
+end
