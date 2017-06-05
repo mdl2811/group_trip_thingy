414104ffc8bb556e1fcc6eb763b52e2759075511
Branden Bird <brandenjbird@gmail.com>
2017-06-05 16:54:17 -0600

    Add state to locations

--- db/migrate/20170604001549_create_locations.rb
+++ db/migrate/20170604001549_create_locations.rb
@@ -2,6 +2,7 @@ class CreateLocations < ActiveRecord::Migration[5.1]
   def change
     create_table :locations do |t|
       t.string :country, null: false
+			t.string :state, null: false
       t.string :city, null: false
       t.string :attraction, null: false
       t.belongs_to :trip, foreign_key: true
