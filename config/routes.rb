82939e1607961b24dc80c91dd3d906c8360c9181
Matthew Peterson <mbpeterson19@gmail.com>
2017-06-05 10:16:48 -0600

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+
 	root 'trips#index'
 	
   devise_for :users
 
 	resources :trips
+
+	resources :locations do
+		resources :addresses
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
